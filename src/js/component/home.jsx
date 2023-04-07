b6f14a84ea984708970c396616818508caae1e2e
raul <raul509@gmail.com>
2023-04-07 01:50:14 -0400

    one

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,50 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
-let Value = [];
+const initialValue = {
+	label: "",
+	done: false,
+};
+
+const urlBase = "http://assets.breatheco.de/apis/fake/todos/user/";
+const apiUsername = "raul88";
 
 const Home = () => {
 
-	const [list, setList] = useState(Value);
-	const [tasks, setTasks] = useState("");
+	const [list, setList] = useState([]);
+	const [tasks, setTasks] = useState(initialValue);
 	const [mistaken, setMistaken] = useState(false);
 
+	// Traer Tareas
+	const fetchTodoApi = async () => {
+		try {
+			const response = await fetch(`${urlBase}${apiUsername}`)
+			const data = await response.json();
+			setList(data);
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	const handleTasksChange = (event) => {
-		setTasks(event.target.value);
-
+		setTasks({ ...tasks, [event.target.name]: event.target.value });
 	};
+
+
+	// Enviar tareas a la api 
+	const updateTasks = async (newlLisTask) => {
+		try {
+			const response = await fetch(`${urlBase}${apiUsername}`, {
+				method: "PUT",
+				headers: {
+					"Content-type": "application/json"
+				},
+				body: JSON.stringify(newlLisTask),
+			});
+			if (response.ok === true) {
+				fetchTodoApi();
+			}
+		} catch (error) {
+			console.log(error);
+		};
+	};
+
 	const handleEnter = (event) => {
 		if (event.key === "Enter") {
-			const newValue = {
-				nombre: tasks
-			};
+			const newValue = tasks;
 			const newlLisTask = [...list, newValue];
-			setList(newlLisTask);
-			setTasks("");
+			updateTasks(newlLisTask);
+			setTasks(initialValue);
 		};
+
 
 	};
 
 	const handleButton = () => {
 
-		if (tasks.trim() === "") {
+		if (tasks.label.trim() === "") {
 			setMistaken(true)
 			return;
 		}
-		const newValue = {
-			nombre: tasks
-		};
+		const newValue = tasks;
 		const newlLisTask = [...list, newValue];
-		setList(newlLisTask);
-		setTasks("");
+		updateTasks(newlLisTask);
+		setTasks(initialValue);
 	};
 
-
-	const hadledRemove = (index) => {
-		setList(list.filter((_, i) => index !== i))
-
+	const hanledButtonRemove = () => {
+		updateTasks([
+			{
+				label: "Make the bed",
+				done: false,
+			}
+		]);
 	};
+
+	const hadledRemove = (id) => {
+		let newlLisTask = list.filter((value, index) => index !== id);
+		updateTasks(newlLisTask);
+	};
+
+	useEffect(() => {
+		fetchTodoApi();
+	}, [])
 
 	return (
 
@@ -53,27 +97,35 @@ const Home = () => {
 			<div className="card border border-dark-subtle" >
 				<div className="card-body d-flex justify-content-center gap-3 ">
 					<input
-					    className="rounded-2 col-10"
-						value={tasks}
+						name="label"
+						className="rounded-2 col-10"
+						value={tasks.label}
 						type="text"
 						placeholder="New Tasks"
-						onChange={(event) => handleTasksChange(event)} 
+						onChange={(event) => handleTasksChange(event)}
 						onKeyDown={(event) => handleEnter(event)}
 					/>
-					<button 
-					className="btn btn-success"
-					onClick={() => handleButton()} ><i className="fas fa-plus"></i></button>
+					<button
+						className="btn btn-success"
+						onClick={() => handleButton()} ><i className="fas fa-plus"></i>
+					</button>
+				</div>
+				<div className=" d-flex justify-content-center">
+					<button
+						className="btn btn-danger"
+						onClick={() => hanledButtonRemove()} >Borrar Lista
+					</button>
 				</div>
 				<div>
-					<h3 className="text-center">{tasks}</h3>
+					<h3 className="text-center">{tasks.label}</h3>
 				</div>
 				<div className="text-center">
 					{list.length === 0 && <h2 className="text-secondary">No tasks, add a task</h2>}
 					<ul className="list-group list-group-flush ">
 						{list.map((producto, index) => {
 							return (
-								<li className="list-group-item d-flex justify-content-between" key={`${producto.nombre}-${index}`}>
-									<h2 className="d-flex justify-content-start">{producto.nombre}</h2>
+								<li className="list-group-item d-flex justify-content-between" key={`${producto}-${index}`}>
+									<h2 className="d-flex justify-content-start">{producto.label}</h2>
 									<button className="btn btn-danger d-flex justify-content-end text-center" onClick={() => hadledRemove(index)}><i className="fas fa-times align-self-center "></i></button>
 								</li>
 							);
@@ -82,10 +134,10 @@ const Home = () => {
 				</div>
 				<div className="card-footer text-muted">
 					{mistaken && <h6 className="link-danger text-center">I miss entering values</h6>}
-					{list.length} item left					
+					{list.length} item left
 				</div>
-				
-				
+
+
 
 			</div>
 		</div>
